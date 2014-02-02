b8803b63838416958041afbb5cc94d29996d96df
jesulink2514 <jesus.angulo@outlook.com>
2014-02-02 18:07:08 -0500

    EF Code First

---
+++ EFCodeFirst/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCodeFirst.Models;
+
+namespace EFCodeFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public PruebaDBContext context =new PruebaDBContext(); 
+
+        public ActionResult Index()
+        {
+            var schools = context.Schools.Where(s=> s.Name.StartsWith("s")).ToList();
+
+
+
+            return View(schools);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
